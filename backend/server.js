cc27d11e1265f839454eaba0c5dff854f7f2d29e
geokal21 <geokalik21@gmail.com>
2026-05-10 01:19:53 +0300

    add authentication routes with signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Server is ready!");
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
